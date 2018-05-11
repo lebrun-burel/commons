47a07b7aa17f223130d421c347883452809a2e88
Eric Burel <e.burel.contact@gmail.com>
2018-05-11 16:57:44 +0200

    transpile

--- lib-es6/test/fixture.js
+++ lib-es6/test/fixture.js
@@ -1,8 +1,6 @@
-'use strict';
+const assert = require('assert');
 
-var assert = require('assert');
-
-var findAllData = [{
+const findAllData = [{
   id: 0,
   description: 'You have to do something'
 }, {
@@ -11,14 +9,15 @@ var findAllData = [{
 }];
 
 exports.Service = {
-  events: ['log'],
+  events: [ 'log' ],
 
-  find: function find() {
+  find () {
     return Promise.resolve(findAllData);
   },
-  get: function get(name, params) {
+
+  get (name, params) {
     if (params.query.error) {
-      return Promise.reject(new Error('Something for ' + name + ' went wrong'));
+      return Promise.reject(new Error(`Something for ${name} went wrong`));
     }
 
     if (params.query.runtimeError) {
@@ -27,11 +26,12 @@ exports.Service = {
 
     return Promise.resolve({
       id: name,
-      description: 'You have to do ' + name + '!'
+      description: `You have to do ${name}!`
     });
   },
-  create: function create(data) {
-    var result = Object.assign({}, data, {
+
+  create (data) {
+    const result = Object.assign({}, data, {
       id: 42,
       status: 'created'
     });
@@ -42,9 +42,10 @@ exports.Service = {
 
     return Promise.resolve(result);
   },
-  update: function update(id, data) {
-    var result = Object.assign({}, data, {
-      id: id, status: 'updated'
+
+  update (id, data) {
+    const result = Object.assign({}, data, {
+      id, status: 'updated'
     });
 
     if (id === null) {
@@ -53,9 +54,10 @@ exports.Service = {
 
     return Promise.resolve(result);
   },
-  patch: function patch(id, data) {
-    var result = Object.assign({}, data, {
-      id: id, status: 'patched'
+
+  patch (id, data) {
+    const result = Object.assign({}, data, {
+      id, status: 'patched'
     });
 
     if (id === null) {
@@ -64,41 +66,47 @@ exports.Service = {
 
     return Promise.resolve(result);
   },
-  remove: function remove(id) {
-    return Promise.resolve({ id: id });
+
+  remove (id) {
+    return Promise.resolve({ id });
   }
 };
 
 exports.verify = {
-  find: function find(data) {
+  find (data) {
     assert.deepEqual(findAllData, data, 'Data as expected');
   },
-  get: function get(id, data) {
+
+  get (id, data) {
     assert.equal(data.id, id, 'Got id in data');
-    assert.equal(data.description, 'You have to do ' + id + '!', 'Got description');
+    assert.equal(data.description, `You have to do ${id}!`, 'Got description');
   },
-  create: function create(original, current) {
+
+  create (original, current) {
     var expected = Object.assign({}, original, {
       id: 42,
       status: 'created'
     });
     assert.deepEqual(expected, current, 'Data ran through .create as expected');
   },
-  update: function update(id, original, current) {
+
+  update (id, original, current) {
     var expected = Object.assign({}, original, {
       id: id,
       status: 'updated'
     });
     assert.deepEqual(expected, current, 'Data ran through .update as expected');
   },
-  patch: function patch(id, original, current) {
+
+  patch (id, original, current) {
     var expected = Object.assign({}, original, {
       id: id,
       status: 'patched'
     });
     assert.deepEqual(expected, current, 'Data ran through .patch as expected');
   },
-  remove: function remove(id, data) {
-    assert.deepEqual({ id: id }, data, '.remove called');
+
+  remove (id, data) {
+    assert.deepEqual({ id }, data, '.remove called');
   }
 };
